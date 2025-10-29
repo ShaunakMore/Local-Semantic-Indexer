c99740719dd6db708627cfea8286d637965cea37
ShaunakMore <shaunaksmore999@gmail.com>
2025-10-29 18:00:44 +0530

    Added multiple image captioning

--- indexer.py
+++ indexer.py
@@ -1,15 +1,50 @@
 import ollama #type:ignore
 from shore_db import ShoreDB #type:ignore
+from pathlib import Path
 from PIL import Image #type:ignore
+import gc
+import pickle
 
-def indexing(model_response,image_path, image_db,image_id_map):
-    id = image_db.random_id_generator()
-    image_id_map[id] = image_path
-    model_response_embedding = ollama.embeddings(
-        model="nomic-embed-text", 
-        prompt=model_response
-    )["embedding"]
-    image_db.add_vector(id,model_response_embedding)
+
+prompt = "Describe this image briefly and factually in one sentence."       
+        
+
+image_arr = ["Images/img1.jpg","Images/img2.jpg","Images/img3.jpg","Images/img4.jpg","Images/img5.jpg","Images/img6.jpg"]
+
+def captioning(images,prompt):
+    responses = []
+    for image in images:
+        try:
+            response = ollama.chat(
+                model='llama3.2-vision:11b',
+                messages=[{
+                    'role': 'user',
+                    'content':prompt,
+                    'images': [image]
+                }]
+            )
+            caption = response["message"]["content"]
+            print("\nDescription by Ollama: \n")
+            print(caption)
+            responses.append(caption)
+        except Exception as e:
+            print(f"\nError processing image:{e}")
+        finally:
+            del image
+            gc.collect()
+    print("\nImages captioned successfully!\n")
+    return responses
+    
+
+def indexing(model_responses,image_paths, image_db,image_id_map):
+    for (image_path,model_response) in zip(image_paths,model_responses):
+        id = image_db.random_id_generator()
+        image_id_map[id] = image_path
+        model_response_embedding = ollama.embeddings(
+            model="nomic-embed-text", 
+            prompt=model_response
+        )["embedding"]
+        image_db.add_vector(id,model_response_embedding)
     return "Image added successfully"
 
 def retrieve_images(query,image_db,image_id_map,k):
@@ -23,39 +58,18 @@ def retrieve_images(query,image_db,image_id_map,k):
         image_path = image_id_map[image_id]
         img_read = Image.open(image_path)
         img_read.show()    
-    
 
-prompt = (
-    "Look at the image and describe it in one short, factual sentence. "
-    "Only mention the main visible objects and their spatial relationship. "
-    "Do not include emotions, moods, or details like lighting or atmosphere. "
-    "Do not use words like 'appears', 'seems', or 'suggests'. "
-    "Format your response like this example: 'a coffee cup on a wooden desk with a laptop'. "
-    "Output only the description."
-)
-
-response = ollama.chat(
-    model='llama3.2-vision:11b',
-    messages=[{
-        'role': 'user',
-        'content':prompt,
-        'images': ['img.png']
-    }]
-)
-
-print("Description by Ollama: \n")
-print(response["message"]["content"]+"\n")
-
-model_response = response["message"]["content"]
-print(f"\nModel response dtype: {type(model_response)}")
 
 db = ShoreDB()
 
 image_id_map = dict()
 
-print(indexing(model_response,"img.png",db,image_id_map))
+model_response = captioning(image_arr,prompt)
+print(indexing(model_response,image_arr,db,image_id_map))
 
-query = input("\nSearch Image:\n")
-print("\n")
 
-retrieve_images(query,db,image_id_map,1)
+
+while(True):   
+    query = input("\nSearch Image:\n")
+    print("\n") 
+    retrieve_images(query,db,image_id_map,1)
