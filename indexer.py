20f4ac821c3243fd31be1409ce656ed4a0aef2d7
ShaunakMore <shaunaksmore999@gmail.com>
2025-10-29 22:04:12 +0530

    Index check and persistance added

--- indexer.py
+++ indexer.py
@@ -8,8 +8,40 @@ import pickle
 
 prompt = "Describe this image briefly and factually in one sentence."       
         
+def check_indexing(folder_path):
+    folder = Path(folder_path)
+    vec_db_path = folder/"index.bin"
+    image_map_path = folder/"image_map.pkl"
+    
+    if vec_db_path.exists() and image_map_path.exists():
+        images = list(folder.glob("*.jpg")) + list(folder.glob("*.jpeg")) + list(folder.glob("*.png"))
+        vec_db = ShoreDB()
+        vec_db.load_from_file(vec_db_path)
+        with open(image_map_path,"rb") as f:
+            image_map = pickle.load(f)
+        paths = list(image_map.values())
+        missing = [image for image in images if image not in paths]
+        if missing:
+            print("\nUnindexed images found. Indexing...")
+            model_response = captioning(missing,prompt=prompt)
+            print(indexing(model_response,missing,vec_db,image_map))
+        return vec_db,image_map
+    
+    else:
+        print("\nFolder not indexed. Indexing...")
+        images = list(folder.glob("*.jpg")) + list(folder.glob("*.jpeg")) + list(folder.glob("*.png"))
+        vec_db = ShoreDB()
+        image_map = {}
+        model_response = captioning(images,prompt=prompt)
+        print(indexing(model_response,images,vec_db,image_map))
+        vec_db.save_to_disk(str(vec_db_path))
+        with open(folder/"image_map.pkl","wb") as f:
+            pickle.dump(image_map,f)
+        print("\nFolder indexed successfully!")
+        return vec_db,image_map
+        
+            
 
-image_arr = ["Images/img1.jpg","Images/img2.jpg","Images/img3.jpg","Images/img4.jpg","Images/img5.jpg","Images/img6.jpg"]
 
 def captioning(images,prompt):
     responses = []
@@ -45,9 +77,10 @@ def indexing(model_responses,image_paths, image_db,image_id_map):
             prompt=model_response
         )["embedding"]
         image_db.add_vector(id,model_response_embedding)
-    return "Image added successfully"
+    return "Image indexed successfully"
 
-def retrieve_images(query,image_db,image_id_map,k):
+def retrieve_images(query,folder,image_db,image_id_map,k):
+    folder_path = Path(folder)
     query_embedding = ollama.embeddings(
         model="nomic-embed-text",
         prompt = query
@@ -55,7 +88,7 @@ def retrieve_images(query,image_db,image_id_map,k):
     retrieved_images = image_db.k_nearest_neighbours(query_embedding,k)
     for image in retrieved_images:
         image_id = image[0]
-        image_path = image_id_map[image_id]
+        image_path = folder_path/image_id_map[image_id]
         img_read = Image.open(image_path)
         img_read.show()    
 
@@ -64,12 +97,11 @@ db = ShoreDB()
 
 image_id_map = dict()
 
-model_response = captioning(image_arr,prompt)
-print(indexing(model_response,image_arr,db,image_id_map))
+folder = "D:/Projects/Semantic Image Indexer/Images"
 
-
+db,image_id_map =check_indexing(folder)
 
 while(True):   
     query = input("\nSearch Image:\n")
     print("\n") 
-    retrieve_images(query,db,image_id_map,1)
+    retrieve_images(query,folder,db,image_id_map,1)
